28e9e464c2cd7ac00f832c6204475e65b02644c4
Infinical <inficalvin@gmail.com>
2023-06-23 09:53:15 +0300

    first commit

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "types"
+
+export const siteConfig: SiteConfig = {
+  name: "Masai Code",
+  description:
+    "Innovative Mobile Development Solutions to Drive Your Business Forward",
+  url: "http://masaicode.com/",
+  ogImage: "",
+  links: {
+    twitter: "",
+    github: "https://github.com/orgs/Masai-Code/dashboard",
+  },
+}
